0583f4aeb8a782d26b70e0a5475298e9a3e268e6
Roshan Kataruka <katarukaroshan@gmail.com>
2023-10-15 14:01:14 +0530

    Authentication Response updated

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -11,7 +11,7 @@ module.exports = {
             {
                 res.json({
                     status :0,
-                    message: "Invalid Token"
+                    LogoutResponse: "Invalid Token"
                 })
             }
             else{
@@ -25,7 +25,7 @@ module.exports = {
         else{
             res.json({
                 status :0,
-                message: "Access denied! unauthorized"
+                unauthResponse: "Access denied! unauthorized"
             });
         }
     }
